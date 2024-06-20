8dbad820af574867179f444d496f8339d44cc3e5
Patrick Schoening <pat.schoening@gmail.com>
2024-06-20 14:03:34 -0500

    updates

--- fileSlacker.py
+++ fileSlacker.py
@@ -1,7 +1,7 @@
 import json
 import logging
-from slack_bolt.adapter.socket_mode import SocketModeHandler
-from slack_bolt import App
+# from slack_bolt.adapter.socket_mode import SocketModeHandler
+# from slack_bolt import App
 
 slack_bolt_app = App()
 
@@ -9,16 +9,14 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-@slack_bolt_app.event("app_mention")
-def handle_app_mention(ack, body, say, client):
+def lambda_handler(event, context):
     try:
-        json_object = json.dumps(body, indent=4)
-        logger.info(json_object)
-        say(json_object)
-    except Exception as e:
-        logger.error(e)
-        say("error")
-
-
-if __name__ == "__main__":
-    SocketModeHandler(slack_bolt_app).start()
+        logger.debug(f'## EVENT\n{str(event)}')
+        logger.debug(f'## CONTEXT\n{str(context)}')
+        # slack_event = json.loads(event["body"])
+        logger.info(event["body"])
+        return {
+            'statusCode': 200,
+        }
+    except Exception as err:
+        logger.error(err)
