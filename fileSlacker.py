4e27eb9eae3d376b343d6eefa8aa8f21c9cc7d81
Pat Schoening <pat_schoening@yahoo.com>
2024-06-24 04:17:18 -0500

    changed to streaming file from slack to aws s3

--- fileSlacker.py
+++ fileSlacker.py
@@ -1,27 +1,30 @@
 import json
 import logging
 import requests
+from botocore.client import Config
+import boto3
+from botocore.exceptions import NoCredentialsError
+import mimetypes
 
-# from slack_bolt.adapter.socket_mode import SocketModeHandler
-# from slack_bolt import App
-
-# slack_bolt_app = App()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+S3_FILE_BUCKET = 'file-slacker-bucket'
+
+
+# S3_METADATA_FOLDER
 
 def lambda_handler(event, context):
     try:
         # logger.debug(f'## EVENT\n{json.dumps(event)}')
         # logger.debug(f'## CONTEXT\n{json.dumps(context)}')
         slack_json = event['body']
-        logger.debug(f"SLACK JSON:\n{slack_json}")
+        logger.info(f"SLACK JSON:\n{slack_json}")
         slack_event = json.loads(slack_json)
         slack_file_url = slack_event['event']['files'][0]['url_private']
-        logger.debug(f"SLACK FILE URL: {slack_file_url}")
-        slack_file = get_slack_file(slack_file_url).content
-        logger.info(type(slack_file))
-        # TODO: push file to S3
+        slack_file_name = slack_event['event']['files'][0]['name']
+        logger.info(f"SLACK FILE URL: {slack_file_url}")
+        upload_file(slack_file_url, S3_FILE_BUCKET, slack_file_name)
         # TODO: push metadata to DB
         return {
             'statusCode': 200
@@ -33,9 +36,18 @@ def lambda_handler(event, context):
         }
 
 
-def get_slack_file(url):
+def upload_file(remote_url, bucket, file_name):
+    cfg = Config(connect_timeout=2, retries={'max_attempts': 0})
+    s3 = boto3.client('s3', config=cfg)
     try:
-        return requests.get(url)
-    except Exception as err:
-        logger.error('Unable to retrieve file via Slack URL.')
+        response = requests.get(remote_url, stream=True).raw
+        content_type = response.headers['content-type']
+        # extension = mimetypes.guess_extension(content_type)
+        s3.upload_fileobj(response, bucket, file_name)
+        print("Upload Successful")
+    except FileNotFoundError:
+        print("The file was not found")
+        raise
+    except NoCredentialsError:
+        print("Credentials not available")
         raise
