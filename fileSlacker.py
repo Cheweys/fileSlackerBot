e5d2f937d4673e5834bb1afb47d2e868a641544f
Patrick Schoening <pat.schoening@gmail.com>
2024-06-24 12:28:43 -0500

    updates to the s3 upload

--- fileSlacker.py
+++ fileSlacker.py
@@ -3,51 +3,71 @@ import logging
 import requests
 from botocore.client import Config
 import boto3
+from io import BytesIO
 from botocore.exceptions import NoCredentialsError
+from botocore.exceptions import ClientError
 import mimetypes
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 S3_FILE_BUCKET = 'file-slacker-bucket'
-
-
 # S3_METADATA_FOLDER
 
 def lambda_handler(event, context):
     try:
-        # logger.debug(f'## EVENT\n{json.dumps(event)}')
-        # logger.debug(f'## CONTEXT\n{json.dumps(context)}')
+        logger.debug(f'## EVENT\n{json.dumps(event)}')
+        logger.debug(f'## CONTEXT\n{str(context)}')
         slack_json = event['body']
-        logger.info(f"SLACK JSON:\n{slack_json}")
+        logger.debug(f"SLACK JSON:\n{slack_json}")
         slack_event = json.loads(slack_json)
         slack_file_url = slack_event['event']['files'][0]['url_private']
         slack_file_name = slack_event['event']['files'][0]['name']
-        logger.info(f"SLACK FILE URL: {slack_file_url}")
-        upload_file(slack_file_url, S3_FILE_BUCKET, slack_file_name)
+        slack_file_mimetype = slack_event['event']['files'][0]['mimetype']
+        logger.debug(f"SLACK FILE URL: {slack_file_url}")
+        logger.debug(f"SLACK FILE NAME: {slack_file_name}")
+        upload_file(slack_file_url, S3_FILE_BUCKET, slack_file_name, slack_file_mimetype)
         # TODO: push metadata to DB
         return {
             'statusCode': 200
         }
     except Exception as err:
-        logger.error(err)
+        logger.error(f"An error occurred.\n{err}")
         return {
             'statusCode': 500
         }
 
 
-def upload_file(remote_url, bucket, file_name):
-    cfg = Config(connect_timeout=2, retries={'max_attempts': 0})
-    s3 = boto3.client('s3', config=cfg)
+def upload_file(remote_url, bucket, file_name, content_type):
+    s3 = boto3.client('s3', 'us-east-2')
     try:
-        response = requests.get(remote_url, stream=True).raw
-        content_type = response.headers['content-type']
-        # extension = mimetypes.guess_extension(content_type)
-        s3.upload_fileobj(response, bucket, file_name)
-        print("Upload Successful")
-    except FileNotFoundError:
-        print("The file was not found")
+        slack_file_response = requests.get(remote_url, stream=True)
+        if slack_file_response.status_code == 200:
+            slack_file = slack_file_response.content
+            # do I need to base64 encode the file before sending to s3?
+            binary_stream = BytesIO(slack_file)
+            s3.upload_fileobj(
+                binary_stream,
+                bucket,
+                file_name,
+                ExtraArgs={'ContentType': content_type})
+        else:
+            raise Exception(f"Unsuccessful HTTP status while fetching Slack file: {slack_file_response.status_code}")
+        '''
+        slack_file_2= requests.get(remote_url, stream=True).raw
+        response = s3.put_object(
+            Body=slack_file_2,
+            Bucket=bucket,
+            Key=file_name+"_2",
+            ContentType=content_type)
+        logger.info('S3 Result 2' + json.dumps(response, indent=2))
+        logger.info("Upload Successful")
+        '''
+    except FileNotFoundError as e:
+        logging.error(f"The file was not found\n{e}")
         raise
-    except NoCredentialsError:
-        print("Credentials not available")
+    except NoCredentialsError as e :
+        logging.error(f"Credentials not available\n{e}")
         raise
+    except ClientError as e:
+        logging.error(f"A Client Error occurred\n{e}")
