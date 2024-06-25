e02f0038b838da83de02dbfd51332e538d9a8d1b
Pat Schoening <pat_schoening@yahoo.com>
2024-06-25 14:13:46 -0500

    cleanup

--- fileSlacker.py
+++ fileSlacker.py
@@ -2,28 +2,27 @@ import json
 import logging
 import requests
 import os
-from botocore.client import Config
 import boto3
+import uuid
 from io import BytesIO
 from botocore.exceptions import NoCredentialsError
 from botocore.exceptions import ClientError
 import mimetypes
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 S3_FILE_BUCKET = 'file-slacker-bucket'
+S3_METADATA_FOLDER = 'file-slacker-bucket/metadata'
 
+s3 = boto3.client('s3', 'us-east-2')
 
-# S3_METADATA_FOLDER
 
 def lambda_handler(event, context):
     try:
         logger.debug(f'## EVENT\n{json.dumps(event)}')
         logger.debug(f'## CONTEXT\n{str(context)}')
-        slack_json = event['body']
-        logger.info(f"SLACK JSON:\n{slack_json}")
-        slack_event = json.loads(slack_json)
+        slack_metadata = build_slack_metadata(event)
 
         # uncomment to verify the Slack Request URL (i.e. for new slack app)
         '''
@@ -34,12 +33,7 @@ def lambda_handler(event, context):
             }
         '''
 
-        slack_file_url = slack_event['event']['files'][0]['url_private']
-        slack_file_name = slack_event['event']['files'][0]['name']
-        slack_file_mimetype = slack_event['event']['files'][0]['mimetype']
-        logger.info(f"SLACK FILE URL: {slack_file_url}")
-        logger.debug(f"SLACK FILE NAME: {slack_file_name}")
-        upload_file(slack_file_url, S3_FILE_BUCKET, slack_file_name, slack_file_mimetype)
+        upload_file_to_s3(slack_metadata['url_private'], S3_FILE_BUCKET, slack_metadata['s3_key'], slack_metadata['mimetype'])
 
         # TODO: push metadata to DB
         return {
@@ -52,13 +46,12 @@ def lambda_handler(event, context):
         }
 
 
-def upload_file(remote_url, bucket, file_name, content_type):
-    s3 = boto3.client('s3', 'us-east-2')
+def upload_file_to_s3(remote_url, bucket, file_name, content_type):
     try:
         slack_token = os.environ["SLACK_BOT_TOKEN"]
         slack_file_response = requests.get(
             remote_url,
-            headers={'Content-Type':'text', 'Authorization': f'Bearer {slack_token}'},
+            headers={'Content-Type': 'text', 'Authorization': f'Bearer {slack_token}'},
             stream=True)
         if slack_file_response.status_code == 200:
             slack_file = slack_file_response.content
@@ -71,10 +64,64 @@ def upload_file(remote_url, bucket, file_name, content_type):
         else:
             raise Exception(f"Unsuccessful HTTP status while fetching Slack file: {slack_file_response.status_code}")
     except FileNotFoundError as e:
-        logging.error(f"The file was not found\n{e}")
+        logging.error(f"The file, {file_name}, was not found\n{e}")
         raise
     except NoCredentialsError as e:
         logging.error(f"Credentials not available\n{e}")
         raise
     except ClientError as e:
         logging.error(f"A Client Error occurred\n{e}")
+        raise
+
+
+def upload_metadata_to_s3():
+    try:
+        '''
+        s3.upload(
+            binary_stream,
+            bucket,
+            file_name,
+            ExtraArgs={'ContentType': content_type})
+        '''
+    except NoCredentialsError as e:
+        logging.error(f"Credentials not available\n{e}")
+        raise
+    except ClientError as e:
+        logging.error(f"A Client Error occurred\n{e}")
+        raise
+
+
+def build_slack_metadata(event):
+    slack_json = event['body']
+    logger.info(f"SLACK JSON:\n{slack_json}")
+    slack_event = json.loads(slack_json)
+    slack_event_file = slack_event['event']['files'][0]
+    md = {
+        'id': f'{slack_event_file['id']}',
+        'created': f'{slack_event_file['created']}',
+        'timestamp': f'{slack_event_file['timestamp']}',
+        'name': f'{slack_event_file['name']}',
+        'mimetype': f'{slack_event_file['mimetype']}',
+        'filetype': f'{slack_event_file['filetype']}',
+        'user': f'{slack_event_file['user']}',
+        'user_team': f'{slack_event_file['user_team']}',
+        'size': f'{slack_event_file['size']}',
+        'url_private': f'{slack_event_file['url_private']}',
+        'user_text': '',
+        's3_key': f'{slack_event_file['id']}.{slack_event_file['filetype']}'
+    }
+    try:
+        blocks = slack_event['event']['blocks']
+        for b in blocks:
+            if b['type'] == 'rich_text':
+                for e1 in b['elements']:
+                    if e1['type'] == 'rich_text_section':
+                        for e2 in e1['elements']:
+                            if e2['type'] == 'text':
+                                md.update({'user_text': f'{e2['text']}'})
+                                break
+                        break
+                break
+    except KeyError:
+        logger.debug('No "user_text" was found.')
+    logger.info(json.dumps(md, indent=4))
